32ff2a692f65e9c0a8aaadc01dbf401b746edfe4
signuldotdev <sanket.prog@gmail.com>
2022-06-30 14:48:16 +0530

    first commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "#black", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <Text>Hi</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
